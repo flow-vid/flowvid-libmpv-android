mpv-android/mpv-android

commit 3cdf7ab2378e398eb480cf2105fb201b9fe75be2

filepicker: clean up path/string conversions

app/src/main/java/is/xyz/mpv/FilePickerActivity.kt
@@ -254,7 +254,9 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
 
         val defaultPathStr = intent.getStringExtra("default_path")
         if (!defaultPathStr.isNullOrEmpty()) {
-            fragment2!!.goToDir(Uri.parse(defaultPathStr))
+            fragment2!!.apply {
+                goToDir(pathFromString(defaultPathStr))
+            }
         }
 
         with (supportFragmentManager.beginTransaction()) {
@@ -297,10 +299,10 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
     private fun finishWithResult(code: Int, path: String? = null) {
         val result = Intent()
         fragment?.apply {
-            result.putExtra("last_path", currentDir.path)
+            result.putExtra("last_path", pathToString(currentDir))
         }
         fragment2?.apply {
-            result.putExtra("last_path", currentDir.toString())
+            result.putExtra("last_path", pathToString(currentDir))
         }
         if (path != null) {
             result.putExtra("path", path)
@@ -316,7 +318,7 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
 
     override fun onDocumentPicked(uri: Uri, isDir: Boolean) {
         if (!isDir)
-            finishWithResult(RESULT_OK, uri.toString())
+            finishWithResult(RESULT_OK, fragment2!!.pathToString(uri))
     }
 
     override fun onCancelled() = finishWithResult(RESULT_CANCELED)
@@ -364,7 +366,7 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
                 // filter hidden files due to stuff like ".thumbnails"
                 contents.filterNot { it.startsWith('.') }.any()
             } else {
-                Utils.MEDIA_EXTENSIONS.contains(file.extension.toLowerCase())
+                Utils.MEDIA_EXTENSIONS.contains(file.extension.lowercase())
             }
         }
 

app/src/main/java/is/xyz/filepicker/AbstractFilePickerFragment.java
@@ -227,7 +227,7 @@ public abstract class AbstractFilePickerFragment<T> extends Fragment
         mFiles = data;
         mAdapter.setList(data);
         onChangePath(mCurrentPath);
-        String key = mCurrentPath.toString();
+        String key = pathToString(mCurrentPath);
         if (mPositionMap.containsKey(key))
             layoutManager.scrollToPositionWithOffset(mPositionMap.get(key), 0);
         else
@@ -328,7 +328,7 @@ public abstract class AbstractFilePickerFragment<T> extends Fragment
      * <p/>
      */
     public void goUp() {
-        String key = mCurrentPath.toString();
+        String key = pathToString(mCurrentPath);
         mPositionMap.remove(key);
         goToDir(getParent(mCurrentPath), false);
     }
@@ -341,7 +341,7 @@ public abstract class AbstractFilePickerFragment<T> extends Fragment
      */
     public void onClickDir(@NonNull View view, @NonNull DirViewHolder viewHolder) {
         if (isDir(viewHolder.file)) {
-            String key = mCurrentPath.toString();
+            String key = pathToString(mCurrentPath);
             mPositionMap.put(key, layoutManager.findFirstVisibleItemPosition());
             goToDir(viewHolder.file, false);
         }

app/src/main/java/is/xyz/filepicker/DocumentPickerFragment.java
@@ -108,12 +108,6 @@ public class DocumentPickerFragment extends AbstractFilePickerFragment<Uri> {
 
     @NonNull
     @Override
-    public Uri toUri(@NonNull Uri path) {
-        return path;
-    }
-
-    @NonNull
-    @Override
     public Uri getParent(@NonNull Uri from) {
         Document doc = mLastRead.get(from);
         if (doc != null) {
@@ -125,13 +119,13 @@ public class DocumentPickerFragment extends AbstractFilePickerFragment<Uri> {
 
     @NonNull
     @Override
-    public String getFullPath(@NonNull Uri path) {
+    public String pathToString(@NonNull Uri path) {
         return path.toString();
     }
 
     @NonNull
     @Override
-    public Uri getPath(@NonNull String path) {
+    public Uri pathFromString(@NonNull String path) {
         return Uri.parse(path);
     }
 

app/src/main/java/is/xyz/filepicker/FilePickerFragment.java
@@ -214,7 +214,7 @@ public class FilePickerFragment extends AbstractFilePickerFragment<File> {
      */
     @NonNull
     @Override
-    public File getPath(@NonNull final String path) {
+    public File pathFromString(@NonNull final String path) {
         return new File(path);
     }
 
@@ -224,7 +224,7 @@ public class FilePickerFragment extends AbstractFilePickerFragment<File> {
      */
     @NonNull
     @Override
-    public String getFullPath(@NonNull final File path) {
+    public String pathToString(@NonNull final File path) {
         return path.getPath();
     }
 
@@ -237,18 +237,6 @@ public class FilePickerFragment extends AbstractFilePickerFragment<File> {
     @Override
     public File getRoot() {
         return new File("/");
-    }
-
-    /**
-     * Convert the path to a URI for the return intent
-     *
-     * @param file either a file or directory
-     * @return a Uri
-     */
-    @NonNull
-    @Override
-    public Uri toUri(@NonNull final File file) {
-        return Uri.fromFile(file);
     }
 
     /**

app/src/main/java/is/xyz/filepicker/LogicHandler.java
@@ -6,7 +6,6 @@
 
 package is.xyz.filepicker;
 
-import android.net.Uri;
 import androidx.annotation.NonNull;
 import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,15 +37,6 @@ public interface LogicHandler<T> {
     String getName(@NonNull final T path);
 
     /**
-     * Convert the path to a URI for the return intent
-     *
-     * @param path
-     * @return a Uri
-     */
-    @NonNull
-    Uri toUri(@NonNull final T path);
-
-    /**
      * Return the path to the parent directory. Should return the root if
      * from is root.
      *
@@ -56,19 +46,16 @@ public interface LogicHandler<T> {
     T getParent(@NonNull final T from);
 
     /**
-     * @param path
-     * @return the full path to the file
+     * Convert path to a string representation.
      */
     @NonNull
-    String getFullPath(@NonNull final T path);
+    String pathToString(@NonNull final T path);
 
     /**
-     * Convert the path to the type used.
-     *
-     * @param path
+     * Convert a string representation back to a path.
      */
     @NonNull
-    T getPath(@NonNull final String path);
+    T pathFromString(@NonNull final String path);
 
     /**
      * Get the root path (lowest allowed).

flow-vid/flowvid-libmpv-android

commit 0e0aafb0c5a4e8865b4fd797b3685d5c3dbd67e7

FilePickerActivity: fix single document picker
closes #897
broken by 3cdf7ab2378e398eb480cf2105fb201b9fe75be2

app/src/main/java/is/xyz/mpv/FilePickerActivity.kt
@@ -32,7 +32,7 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
 
     private var documentOpener = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
         it?.let { uri ->
-            onDocumentPicked(uri, false)
+            finishWithResult(RESULT_OK, uri.toString())
         }
     }
 
@@ -319,6 +319,7 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
     override fun onDirPicked(dir: File) = finishWithResult(RESULT_OK, dir.absolutePath)
 
     override fun onDocumentPicked(uri: Uri, isDir: Boolean) {
+        assert(fragment2 != null)
         if (!isDir)
             finishWithResult(RESULT_OK, fragment2!!.pathToString(uri))
     }