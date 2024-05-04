mpv-android/mpv-android

commit ae6c3ca65bd127752377944d6acb8c998ee6e185

MPVActivity: cancel old toast when switching track

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -871,7 +871,9 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
     private fun playlistPrev() = MPVLib.command(arrayOf("playlist-prev"))
     private fun playlistNext() = MPVLib.command(arrayOf("playlist-next"))
 
-    private fun showToast(msg: String) {
+    private fun showToast(msg: String, cancel: Boolean = false) {
+        if (cancel)
+            toast.cancel()
         toast.setText(msg)
         toast.show()
     }
@@ -964,13 +966,13 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
             else    -> "???"
         }
 
-        if (track_id == -1) {
-            showToast("$trackPrefix ${getString(R.string.track_off)}")
-            return
+        val msg = if (track_id == -1) {
+            "$trackPrefix ${getString(R.string.track_off)}"
+        } else {
+            val trackName = player.tracks[track_type]?.firstOrNull{ it.mpvId == track_id }?.name ?: "???"
+            "$trackPrefix $trackName"
         }
-
-        val trackName = player.tracks[track_type]?.firstOrNull{ it.mpvId == track_id }?.name ?: "???"
-        showToast("$trackPrefix $trackName")
+        showToast(msg, true)
     }
 
     private fun cycleAudio() = trackSwitchNotification {

flow-vid/flowvid-libmpv-android

commit 9bd41b73e42fbddca354e34ccaed9b59b91a3f52

MPVActivity: fix track switch toasts not appearing
not sure why this started happening now

fixes: ae6c3ca65bd127752377944d6acb8c998ee6e185

app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -61,8 +61,9 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
 
     private var activityIsForeground = true
     private var didResumeBackgroundPlayback = false
-
     private var userIsOperatingSeekbar = false
+
+    private var toast: Toast? = null
 
     private var audioManager: AudioManager? = null
     private var audioFocusRestore: () -> Unit = {}
@@ -71,7 +72,6 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
     private var mediaSession: MediaSessionCompat? = null
 
     private lateinit var binding: PlayerBinding
-    private lateinit var toast: Toast
     private lateinit var gestures: TouchGestures
 
     // convenience alias
@@ -234,12 +234,6 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         }
     }
 
-    @SuppressLint("ShowToast")
-    private fun initMessageToast() {
-        toast = Toast.makeText(this, "This totally shouldn't be seen", Toast.LENGTH_SHORT)
-        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 0)
-    }
-
     private var playbackHasStarted = false
     private var onloadCommands = mutableListOf<Array<String>>()
 
@@ -260,9 +254,6 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
 
         // Initialize listeners for the player view
         initListeners()
-
-        // Initialize toast used for short messages
-        initMessageToast()
 
         gestures = TouchGestures(this)
 
@@ -898,9 +889,11 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
 
     private fun showToast(msg: String, cancel: Boolean = false) {
         if (cancel)
-            toast.cancel()
-        toast.setText(msg)
-        toast.show()
+            toast?.cancel()
+        toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 0)
+            show()
+        }
     }
 
     // Intent/Uri parsing