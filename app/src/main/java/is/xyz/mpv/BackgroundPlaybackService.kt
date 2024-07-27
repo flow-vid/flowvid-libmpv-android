mpv-android/mpv-android

commit e43a7283236faee26648d0f5e54f09ea1f114520

app: raise target sdk to 34

app/src/main/java/is/xyz/mpv/BackgroundPlaybackService.kt
@@ -14,6 +14,7 @@ import androidx.annotation.StringRes
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.ServiceCompat
 import androidx.media.app.NotificationCompat.MediaStyle
 
 /*
@@ -109,7 +110,7 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
         // create notification and turn this into a "foreground service"
 
         val notification = buildNotification()
-        startForeground(NOTIFICATION_ID, notification)
+        ServiceCompat.startForeground(this, NOTIFICATION_ID, notification, 0)
 
         return START_NOT_STICKY // Android can't restart this service on its own
     }

app/build.gradle
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         minSdkVersion 21
-        targetSdkVersion 33
+        targetSdkVersion 34
 
         versionCode 37
         versionName "2024-06-05-release"

app/src/main/AndroidManifest.xml
@@ -12,6 +12,7 @@
 
     <uses-permission android:name="android.permission.INTERNET" />
     <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
+    <uses-permission android:name="android.permission.FOREGROUND_SERVICE_MEDIA_PLAYBACK" />
 
     <application
         android:allowBackup="true"

app/src/main/java/is/xyz/mpv/FilePickerActivity.kt
@@ -218,13 +218,13 @@ class FilePickerActivity : AppCompatActivity(), AbstractFilePickerFragment.OnFil
         }
     }
 
-    override fun dispatchKeyEvent(ev: KeyEvent?): Boolean {
+    override fun dispatchKeyEvent(ev: KeyEvent): Boolean {
         // If up is pressed at the header element display the usual options menu as a popup menu
         // to make it usable on Android TV.
         var openMenu = false
         if (fragment == null) {
             // only for file picker
-        } else if (ev?.action == KeyEvent.ACTION_DOWN && ev.keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+        } else if (ev.action == KeyEvent.ACTION_DOWN && ev.keyCode == KeyEvent.KEYCODE_DPAD_UP) {
             val recycler: RecyclerView = findViewById(android.R.id.list)
             val holder = try {
                 window.currentFocus?.let { recycler.getChildViewHolder(it) }

flow-vid/flowvid-libmpv-android

commit 2a162ff80782be936092fc72c940565eef4e9598

BackgroundPlaybackService: fix missing service type for newer API levels
I could have sworn that I read somewhere startForeground() would read it
from the manifest automatically.

fixes: e43a7283236faee26648d0f5e54f09ea1f114520

app/src/main/java/is/xyz/mpv/BackgroundPlaybackService.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.IBinder
 import android.support.v4.media.session.MediaSessionCompat
 import android.util.Log
@@ -43,7 +45,6 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
         }
     }
 
-    @SuppressLint("UnspecifiedImmutableFlag")
     private fun buildNotification(): Notification {
         val notificationIntent = Intent(this, MPVActivity::class.java)
         val pendingIntent = PendingIntentCompat.getActivity(this, 0, notificationIntent, 0, false)
@@ -95,6 +96,12 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
         return builder.build()
     }
 
+    @SuppressLint("NotificationPermission") // not required for foreground service
+    private fun refreshNotification() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, buildNotification())
+    }
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.v(TAG, "BackgroundPlaybackService: starting")
 
@@ -107,7 +114,12 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
         // create notification and turn this into a "foreground service"
 
         val notification = buildNotification()
-        ServiceCompat.startForeground(this, NOTIFICATION_ID, notification, 0)
+        val type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+        } else {
+            0
+        }
+        ServiceCompat.startForeground(this, NOTIFICATION_ID, notification, type)
 
         return START_NOT_STICKY // Android can't restart this service on its own
     }
@@ -128,9 +140,7 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
         if (property != "pause")
             return
         paused = value
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(NOTIFICATION_ID, buildNotification())
+        refreshNotification()
     }
 
     override fun eventProperty(property: String, value: Long) { }
@@ -138,9 +148,7 @@ class BackgroundPlaybackService : Service(), MPVLib.EventObserver {
     override fun eventProperty(property: String, value: String) {
         if (!cachedMetadata.update(property, value))
             return
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(NOTIFICATION_ID, buildNotification())
+        refreshNotification()
     }
 
     override fun event(eventId: Int) {