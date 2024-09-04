0b99aa490696975cc636a0fc686bbd28adaf4f7d
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-09-04 07:47:04 +0100

    fix ffmpeg version in Utils

--- buildscripts/scripts/write_versions.sh
+++ buildscripts/scripts/write_versions.sh
@@ -2,7 +2,7 @@
 # get versions from source code
 MPV_VERSION=$(cat buildscripts/deps/mpv/_build$1/common/version.h | grep "#define VERSION" | cut -d '"' -f 2)
 LIBPLACEBO_VERSION=$(cat buildscripts/deps/libplacebo/_build$1/src/version.h | grep "#define BUILD_VERSION" | cut -d '"' -f 2)
-FFMPEG_VERSION=$(cat buildscripts/include/depinfo.sh | grep "v_ffmpeg=" | cut -d '=' -f 2)
+FFMPEG_VERSION=$(echo $(cd buildscripts/deps/ffmpeg/ && git rev-parse --short HEAD))
 # get build date from compiled object file
 START_RODATA=0x$(readelf buildscripts/deps/mpv/_build$1/libmpv.so.p/common_version.c.o -S | grep .rodata | cut -d ' ' -f 27)
 START=0x$(readelf buildscripts/deps/mpv/_build$1/libmpv.so.p/common_version.c.o -s | grep mpv_builddate | cut -d ' ' -f 7)
